b95c8a918056bca441084bd9cf084dafd4877ff9
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-07-11 17:36:02 -0300

    Refactored homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import Home from "@/components/home/Home";
+import Verse from "@/components/chapter/Verse";
+import { VersionLink } from "@/components/home/VersionLink";
+import { api } from "@/services/api";
 
-export default function HomePage() {
-  return <Home />;
+export default async function HomePage() {
+  const [verse, versions] = await Promise.all([
+    api.getVerse("nvi", "hb", 4, 12),
+    api.getVersions(),
+  ]);
+
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl pb-4">Versículo do dia</h1>
+        <Verse className="pb-2" text={verse.text} />
+        <div className="text-right">Hebreus 4:12</div>
+      </div>
+      <div>
+        <h2 className="text-2xl py-3">Versões</h2>
+        {versions.map((version, i) => {
+          return <VersionLink version={version} key={i} />;
+        })}
+      </div>
+    </>
+  );
 }
