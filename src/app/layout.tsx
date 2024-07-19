d8e99c88fdef1797d7bcd2184e07ff6ab4f9f6f0
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-07-19 17:37:01 -0300

    PWA configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Bíblia",
   description: "Bíblia",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
