e51581d191709e8736e6a8229c53ee289b1c5cbd
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-08-07 17:09:41 -0300

    New PWA configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  disable: process.env.NODE_ENV === "development",
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSerwist(nextConfig);
