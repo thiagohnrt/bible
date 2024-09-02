27cc28c16e9da98c93af10d0a0be097b4eeba017
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-09-02 11:15:10 -0300

    Verse of Day with image

--- src/components/home/VerseOfDay.tsx
+++ src/components/home/VerseOfDay.tsx
@@ -4,6 +4,7 @@ import { BibleContext } from "@/providers/bibleProvider";
 import { api, Verse as IVerse } from "@/services/api";
 import { useContext, useEffect, useState } from "react";
 import Verse from "../chapter/Verse";
+import { cn } from "@/lib/utils";
 
 interface Props {
   className?: string;
@@ -25,9 +26,17 @@ export function VerseOfDay({ className }: Props) {
 
   return (
     <div className={className}>
-      <h1 className="text-lg font-bold">Versículo do dia</h1>
-      <div className="text-lg py-4">Hebreus 4:12 - {translation?.short_name}</div>
-      <Verse className="pb-2" text={verse.text} />
+      <h1 className="text-lg font-bold mb-2">Versículo do dia</h1>
+      <div
+        className={cn("flex flex-col gap-4 text-center p-6 rounded-lg", "bg-center bg-cover bg-blend-saturation")}
+        style={{
+          backgroundImage: "linear-gradient(black, black), url('/img/verseOfDay/1.jpg')",
+          textShadow: "1px 1px 5px #000",
+        }}
+      >
+        <Verse className="pb-2 text-base leading-9" text={verse.text} />
+        <div className="text-sm">Hebreus 4:12 </div>
+      </div>
     </div>
   );
 }
