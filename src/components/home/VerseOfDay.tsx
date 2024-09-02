1c72465563e8ef9db5a8787f0c9cb8c15d57695c
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-09-02 18:01:09 -0300

    Expand image verse of Day

--- src/components/home/VerseOfDay.tsx
+++ src/components/home/VerseOfDay.tsx
@@ -5,6 +5,16 @@ import { api, Verse as IVerse } from "@/services/api";
 import { useContext, useEffect, useState } from "react";
 import Verse from "../chapter/Verse";
 import { cn } from "@/lib/utils";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { MdMonochromePhotos, MdShare } from "react-icons/md";
 
 interface Props {
   className?: string;
@@ -13,6 +23,7 @@ interface Props {
 export function VerseOfDay({ className }: Props) {
   const { translation } = useContext(BibleContext);
   const [verse, setVerse] = useState<IVerse | null>(null);
+  const [isMonochrome, setMonochrome] = useState(true);
 
   useEffect(() => {
     if (translation) {
@@ -29,6 +40,14 @@ export function VerseOfDay({ className }: Props) {
     return `/img/verseOfDay/${(dayOfYear % max) + 1}.jpg`;
   };
 
+  const classNameImage = `flex flex-col justify-center gap-4 text-center text-white py-6 px-8 bg-center bg-cover ${
+    isMonochrome ? "bg-blend-saturation" : ""
+  }`;
+  const styleImage = {
+    backgroundImage: `${isMonochrome ? "linear-gradient(black, black)," : ""} url(${randomImage(5)})`,
+    textShadow: "1px 1px 5px #000",
+  };
+
   if (!verse) {
     return <></>;
   }
@@ -36,19 +55,35 @@ export function VerseOfDay({ className }: Props) {
   return (
     <div className={className}>
       <h1 className="text-lg font-bold mb-2">Versículo do dia</h1>
-      <div
-        className={cn(
-          "flex flex-col justify-center gap-4 text-center text-white py-6 px-8 min-h-80 rounded-lg",
-          "bg-center bg-cover bg-blend-saturation"
-        )}
-        style={{
-          backgroundImage: `linear-gradient(black, black), url(${randomImage(5)})`,
-          textShadow: "1px 1px 5px #000",
-        }}
-      >
-        <Verse className="pb-2 text-base leading-9" text={verse.text} />
-        <div className="text-sm">Hebreus 4:12 </div>
-      </div>
+      <Dialog id="verseOfDay">
+        <DialogTrigger>
+          <div className={cn("min-h-80 rounded-lg", classNameImage)} style={styleImage}>
+            <Verse className="pb-2 text-base leading-9" text={verse.text} />
+            <div className="text-sm">Hebreus 4:12</div>
+          </div>
+        </DialogTrigger>
+        <DialogContent className="h-svh w-lvw p-0">
+          <DialogHeader className="hidden">
+            <DialogTitle></DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div className={cn("", classNameImage)} style={styleImage}>
+            <Verse className="pb-2 text-base leading-9" text={verse.text} />
+            <div className="text-sm">Hebreus 4:12</div>
+          </div>
+          <DialogFooter className="absolute left-0 right-0 bottom-0 flex flex-row justify-center gap-8 p-8 text-white">
+            <div className="flex items-center justify-center w-12 h-12 rounded-full active:shadow-xl transition-shadow">
+              <MdShare size={20} />
+            </div>
+            <div
+              className="flex items-center justify-center w-12 h-12 rounded-full active:shadow-xl transition-shadow"
+              onClick={() => setMonochrome(!isMonochrome)}
+            >
+              <MdMonochromePhotos size={20} />
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
