ae7a38d7b48a7f5deadd1c4a8f36fb74dc470d12
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-12-13 08:53:05 -0300

    Fix import

--- src/app/bible/[version]/page.tsx
+++ src/app/bible/[version]/page.tsx
@@ -1,14 +1,16 @@
+"use client";
+
 import { Books } from "@/components/book/Books";
 import { Container } from "@/components/root/Container";
-import { headers } from "next/headers";
-import { userAgent } from "next/server";
+import { RootContext } from "@/providers/rootProvider";
+import { useContext } from "react";
 
 interface Props {
   params: { version: string };
 }
 
 export default function VersionPage({ params: { version } }: Props) {
-  const { device } = userAgent({ headers: headers() });
+  const { device } = useContext(RootContext);
   return (
     <Container>
       <Books version={version} device={device} />
