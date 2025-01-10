a5b4122da3108293768668391235ff5841cce23d
Thiago Honorato <thiagohbhonorato@gmail.com>
2025-01-10 09:51:47 -0300

    Refactor Books component state management for clarity and structure

--- src/components/book/Books.tsx
+++ src/components/book/Books.tsx
@@ -19,7 +19,10 @@ interface Props {
 
 export function Books({ version, device }: Props) {
   const [books, setBooks] = useState<Book[]>([]);
-  const [data, setData] = useState<{ bookId: string; chapter: number }>({ bookId: "", chapter: 0 });
+  const [data, setData] = useState<{ book: string; current: { book: number; chapter: number } }>({
+    book: "",
+    current: { book: 0, chapter: 0 },
+  });
   const itemRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
   const pathname = usePathname();
 
@@ -29,17 +32,11 @@ export function Books({ version, device }: Props) {
 
   useEffect(() => {
     if (books.length) {
-      const bookId = location.search.substring(1).split("-")[0];
+      const book = location.search.substring(1).split("-")[0];
       const chapter = +location.search.substring(1).split("-")[1];
-      if (itemRefs.current[`${bookId}`]) {
-        setTimeout(
-          (ref) => {
-            setData({ bookId, chapter });
-            ref?.click();
-          },
-          250,
-          itemRefs.current[`${bookId}`]
-        );
+      if (itemRefs.current[`${book}`]) {
+        setData({ book, current: { book: +book, chapter } });
+        itemRefs.current[`${book}`]?.click();
       }
     }
   }, [books, pathname]);
@@ -63,8 +60,8 @@ export function Books({ version, device }: Props) {
       type="single"
       collapsible
       className="w-full"
-      value={data.bookId}
-      onValueChange={(value) => setData({ ...data, bookId: value })}
+      value={data.book}
+      onValueChange={(value) => setData({ ...data, book: value })}
     >
       {books.map((book, i) => {
         return (
@@ -81,7 +78,7 @@ export function Books({ version, device }: Props) {
               <Chapters
                 version={version}
                 book={book}
-                chapter={book.book === +data.bookId ? data.chapter : 0}
+                chapter={book.book === data.current.book ? data.current.chapter : 0}
                 device={device}
               />
             </AccordionContent>
