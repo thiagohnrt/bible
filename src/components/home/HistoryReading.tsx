04cc347269c614305236d460a9ed220f5484da6e
Thiago Honorato <thiagohbhonorato@gmail.com>
2025-07-03 07:44:19 -0300

    feat: update HistoryReading component with improved styling and link functionality for reading history items

--- src/components/home/HistoryReading.tsx
+++ src/components/home/HistoryReading.tsx
@@ -20,8 +20,8 @@ export function HistoryReading({ className }: Props) {
     setBibleHistory(data);
   }, []);
   return (
-    <div className={cn("flex flex-col rounded-md bg-highlight p-4", className)}>
-      <div className="flex items-center gap-2">
+    <div className={cn("flex flex-col rounded-md bg-highlight py-4 px-2", className)}>
+      <div className="flex items-center gap-2 px-2">
         <p>Histórico de leitura</p>
         <MdHistory size={18} />
       </div>
@@ -30,15 +30,22 @@ export function HistoryReading({ className }: Props) {
           <CarouselContent className="conteudo h-full">
             {bibleHistory.slice(1, 10).map((item, index) => (
               <CarouselItem key={`bible-history-item-${index}`} className="h-full">
-                <div className="py-1 h-full">
-                  <p className="text-lg font-medium pb-3">
-                    {item.book.name} {item.chapter}:{item.verse?.verse ?? 1} {item.translation}
-                  </p>
-                  <Verse
-                    text={bibleUtils.versesToString([{ text: item.verse.text } as IVerse], { withNumb: false })}
-                    formatting="text"
-                    classNameVerse="line-clamp-2 text-base leading-7"
-                  />
+                <div className="px-2 rounded-md bg-highlight-active h-full">
+                  <Link
+                    href={`/bible/${item.translationId}/${item.book.id}/${item.chapter}${
+                      item.verse?.verse ? `/${item.verse.verse}` : ""
+                    }`}
+                    className="py-1 h-full "
+                  >
+                    <p className="text-lg font-medium pb-3 ">
+                      {item.book.name} {item.chapter}:{item.verse?.verse ?? 1} {item.translation}
+                    </p>
+                    <Verse
+                      text={bibleUtils.versesToString([{ text: item.verse.text } as IVerse], { withNumb: false })}
+                      formatting="text"
+                      classNameVerse="line-clamp-2 text-base leading-7"
+                    />
+                  </Link>
                 </div>
               </CarouselItem>
             ))}
