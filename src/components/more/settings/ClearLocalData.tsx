0da98ae5cf71af260d8844407aa9682d7028c203
Thiago Honorato <thiagohbhonorato@gmail.com>
2025-08-29 18:00:33 -0300

    feat: update version to 0.48.0, add CleaningUpLocalData component, and enhance ClearLocalData with progress tracking

--- src/components/more/settings/ClearLocalData.tsx
+++ src/components/more/settings/ClearLocalData.tsx
@@ -1,8 +1,11 @@
+import { DialogConfirm } from "@/components/root/DialogConfirm";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -10,6 +13,7 @@ import {
 import { db } from "@/database/bibleDB";
 import { DBInfo } from "@/database/indexedDB";
 import React, { useEffect, useState } from "react";
+import { CleaningUpLocalData } from "./CleaningUpLocalData";
 
 interface Props {
   children: React.ReactNode;
@@ -23,6 +27,7 @@ interface SessionData {
 
 export function ClearLocalData({ children }: Props) {
   const [database, setDatabase] = useState<DBInfo>();
+  const [startCleaning, setStartCleaning] = useState(false);
 
   const getSession = (): SessionData[] => {
     const data: SessionData[] = [];
@@ -43,58 +48,78 @@ export function ClearLocalData({ children }: Props) {
     return data;
   };
 
+  const sessionData = getSession();
+
   useEffect(() => {
     db.getDBInfo().then((db) => setDatabase(db));
   }, []);
 
+  useEffect(() => {
+    if (!startCleaning) {
+      db.getDBInfo().then((db) => setDatabase(db));
+    }
+  }, [startCleaning]);
+
   return (
-    <Dialog id="manage-offline-translations">
-      <DialogTrigger asChild>
-        <div>{children}</div>
-      </DialogTrigger>
-      <DialogContent className="flex flex-col h-svh w-lvw p-0 md:max-w-lg md:h-auto md:max-h-[90vh] md:border md:rounded-lg">
-        <DialogHeader className="p-6 pb-3">
-          <DialogTitle>Limpar Dados Local</DialogTitle>
-          <DialogDescription></DialogDescription>
-        </DialogHeader>
-        <div className="px-4 sm:px-6 pb-6 overflow-y-auto">
-          <h2 className="text-lg">Sessão</h2>
-          <Accordion type="multiple" className="w-full">
-            {getSession().map((data, i) => {
-              return (
-                <AccordionItem value={data.name} key={i}>
-                  <AccordionTrigger className="w-full text-left">{data.name}</AccordionTrigger>
-                  <AccordionContent>
-                    <pre className="overflow-x-auto bg-primary/10 p-2 rounded-md">{data.value}</pre>
-                  </AccordionContent>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
-          <div className="py-4"></div>
-          <h2 className="text-lg">Banco de Dados</h2>
-          {!database ? (
-            <div>Calculando...</div>
-          ) : (
-            <div className="grid grid-cols-3 [&>*]:border [&>*]:p-2">
-              <div>Tabela</div>
-              <div>Registros</div>
-              <div>Tamanho</div>
-              {database.tables.map((table, i) => {
+    <React.Fragment>
+      <Dialog id="manage-offline-translations">
+        <DialogTrigger asChild>
+          <div>{children}</div>
+        </DialogTrigger>
+        <DialogContent className="flex flex-col h-svh w-lvw p-0 md:max-w-lg md:h-auto md:max-h-[90vh] md:border md:rounded-lg">
+          <DialogHeader className="p-6 pb-3">
+            <DialogTitle>Limpar Dados Local</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div className="px-4 sm:px-6 pb-6 overflow-y-auto">
+            <h2 className="text-xl">Sessão</h2>
+            {sessionData.length === 0 && <div className="py-3">Nenhum dado na sessão.</div>}
+            <Accordion type="multiple" className="w-full">
+              {sessionData.map((data, i) => {
                 return (
-                  <React.Fragment key={i}>
-                    <div>{table.storeName}</div>
-                    <div>{table.count}</div>
-                    <div>{table.estimatedSizeFormatted}</div>
-                  </React.Fragment>
+                  <AccordionItem value={data.name} key={i}>
+                    <AccordionTrigger className="w-full text-left">{data.name}</AccordionTrigger>
+                    <AccordionContent>
+                      <pre className="overflow-x-auto bg-primary/10 p-2 rounded-md">{data.value}</pre>
+                    </AccordionContent>
+                  </AccordionItem>
                 );
               })}
-              <div className="col-span-2">Total</div>
-              <div>{database.estimatedTotalSizeFormatted}</div>
-            </div>
-          )}
-        </div>
-      </DialogContent>
-    </Dialog>
+            </Accordion>
+            <h2 className="text-xl py-4">Banco de Dados</h2>
+            {!database ? (
+              <div>Calculando...</div>
+            ) : (
+              <div className="grid grid-cols-3 [&>*]:border [&>*]:p-2">
+                <div>Tabela</div>
+                <div>Registros</div>
+                <div>Tamanho</div>
+                {database.tables.map((table, i) => {
+                  return (
+                    <React.Fragment key={i}>
+                      <div>{table.storeName}</div>
+                      <div>{table.count}</div>
+                      <div>{table.estimatedSizeFormatted}</div>
+                    </React.Fragment>
+                  );
+                })}
+                <div className="col-span-2">Total</div>
+                <div>{database.estimatedTotalSizeFormatted}</div>
+              </div>
+            )}
+          </div>
+          <DialogFooter className="p-6 pt-3">
+            <DialogConfirm
+              message="Tem certeza que deseja limpar todos os dados locais?"
+              btnAction="destructive"
+              onConfirm={() => setStartCleaning(true)}
+            >
+              <Button variant={"destructive"}>Limpar Dados Local</Button>
+            </DialogConfirm>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <CleaningUpLocalData open={startCleaning} onOpenChange={setStartCleaning} />
+    </React.Fragment>
   );
 }
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -146,7 +146,7 @@ const DialogTitle = React.forwardRef<
     >
       <div
         onClick={router.back}
-        className="sm:hidden block absolute px-4 py-6 left-0 top-0 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground z-50"
+        className="btn-close sm:hidden block absolute px-4 py-6 left-0 top-0 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground z-50"
       >
         <HiArrowNarrowLeft className="h-4 w-4" />
         <span className="sr-only">Back</span>
